37035b4e03101b8e49de2f62a482a212c2566153
Grayholm <rodiongrayholm@gmail.com>
2025-11-03 04:29:45 +0300

    m2m

--- src/models/exercises.py
+++ src/models/exercises.py
@@ -1,10 +1,13 @@
-
+import typing
 
 from sqlalchemy import Integer, String, Enum
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from src.core.db import Base
 from src.schemas.exercises import Category
+
+if typing.TYPE_CHECKING:
+    from src.models.workouts import WorkoutModel
 
 
 class ExerciseModel(Base):
@@ -14,3 +17,8 @@ class ExerciseModel(Base):
     name: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(String(500))
     category: Mapped[Category] = mapped_column(Enum(Category))
+
+    workouts: Mapped[list["WorkoutModel"]] = relationship(
+        back_populates="exercises",
+        secondary="workout_exercises",
+    )
